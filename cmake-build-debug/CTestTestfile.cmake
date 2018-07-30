043616b8f328af15514dbbb111e50c6013983d2e
zk8888 <1441966581@qq.com>
2018-07-30 15:51:23 +0800

    删除了cmake-build-debug

--- cmake-build-debug/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/zk8888/catkin_ws/src/kinect_img_cap
-# Build directory: /home/zk8888/catkin_ws/src/kinect_img_cap/cmake-build-debug
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("gtest")
